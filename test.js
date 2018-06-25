11c87af862c6528761648883f682dfe027b1b736
フイントゥアン <huynh-t@pise.co.jp>
2018-06-25 11:34:47 +0900

    self

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-window.onload = function (e) {
-    liff.init(function (data) {
-        initializeApp(data);
-    });
-};
-function initializeApp(data) {
-    liff.getProfile().then(function (result) {
-        var app = new Vue({
-      el: '#app',
-      data: {
-        language: data.language,
-        contextType: data.context.type,
-        viewType: data.context.viewType,
-        userId: data.context.userId,
-        name: result.displayName
-      }
-    })
-    });
-}
+// window.onload = function (e) {
+//     liff.init(function (data) {
+//         initializeApp(data);
+//     });
+// };
+// function initializeApp(data) {
+//     liff.getProfile().then(function (result) {
+//         var app = new Vue({
+//       el: '#app',
+//       data: {
+//         language: data.language,
+//         contextType: data.context.type,
+//         viewType: data.context.viewType,
+//         userId: data.context.userId,
+//         name: result.displayName
+//       }
+//     })
+//     });
+// }
